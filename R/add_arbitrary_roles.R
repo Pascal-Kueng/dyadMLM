a793367f82c4da549d78d2f9163f6e5145568d1c
Pascal-Kueng <kueng.pascal@gmail.com>
2026-07-04 00:17:56 +0200

    Update add_arbitrary_roles.R

--- R/add_arbitrary_roles.R
+++ R/add_arbitrary_roles.R
@@ -4,8 +4,8 @@
 #' @param seed Optional seed for random arbitrary partner-role assignment. If
 #'   `NULL`, the current R session's RNG state is used.
 #'
-#' @return The input data with columns .i_arbitrary_role, .i_is_arbitrary_role1,
-#' .i_is_arbitrary_role2, .i_diff which can be used in models.
+#' @return The input data with columns .i_arbitrary_role, .i_is_arbitrary_role_1,
+#' .i_is_arbitrary_role_2, .i_diff which can be used in models.
 #' @keywords internal
 add_arbitrary_roles <- function(data, seed = NULL) {
   if (!inherits(data, "interdep_data")) {
@@ -16,7 +16,6 @@ add_arbitrary_roles <- function(data, seed = NULL) {
 
   group_name <- meta_data$group
   member_name <- meta_data$member
-  time_name <- meta_data$time
 
   # Handle the seed: apply it and make sure it restores when the function exits!
   if (!is.null(seed)) {
@@ -37,6 +36,48 @@ add_arbitrary_roles <- function(data, seed = NULL) {
   }
 
   # create the random roles columns we need.
+  random_member_roles <- data |>
+    # Collapse repeated ILD rows to one role per member.
+    dplyr::distinct(
+      .data[[group_name]],
+      .data[[member_name]]
+    ) |>
+    dplyr::arrange(
+      .data[[group_name]],
+      .data[[member_name]]
+    ) |>
+    dplyr::group_by(.data[[group_name]]) |>
+    dplyr::mutate(
+      .i_arbitrary_role = sample(
+        c("arbitrary_role_1", "arbitrary_role_2"),
+        # if we have a dyad where only 1 member is ever observed, we only sample
+        # 1 value, otherwise both!
+        size = dplyr::n()
+      ),
+
+      .i_is_arbitrary_role_1 = ifelse(
+        .i_arbitrary_role == "arbitrary_role_1",
+        1,
+        0
+      ),
+
+      .i_is_arbitrary_role_1 = as.numeric(.i_arbitrary_role == "arbitrary_role_1"),
+
+      .i_is_arbitrary_role_2 = as.numeric(.i_arbitrary_role == "arbitrary_role_2"),
+
+      .i_diff = ifelse(
+        .i_is_arbitrary_role_1 == 1,
+        -1,
+        1
+      )
+    ) |>
+    dplyr::ungroup()
+
+  data <- dplyr::left_join(
+    data,
+    random_member_roles,
+    by = c(group_name, member_name)
+  )
 
   data
 }
