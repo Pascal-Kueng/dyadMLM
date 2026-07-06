9bc8f4d4b73d7e8dbe384ed90d3f175ee4a3846d
Pascal-Kueng <kueng.pascal@gmail.com>
2026-07-06 02:33:18 +0200

    cleaned up code for pringtin method

--- R/print_interdep_data.R
+++ R/print_interdep_data.R
@@ -25,30 +25,46 @@ print.interdep_data <- function(x, ...) {
   if (!is.null(time)) {
     cat(", time = ", time, sep = "")
   }
-  cat("\n\n")
+  cat("\n#\n")
 
 
 
-  cat("# Dyad Composition: \n")
+  cat("# Dyad compositions:\n")
 
   composition <- format(dyad_compositions$composition, justify = "left")
   dyad_type <- format(dyad_compositions$dyad_type, justify = "left")
-  n_dyads <- format(dyad_compositions$n_dyads, justify = "right")
+  composition_n_dyads <- format(dyad_compositions$n_dyads, justify = "right")
 
-  for (i in 1:nrow(dyad_compositions)) {
-    cat("# ",
+  for (i in seq_len(nrow(dyad_compositions))) {
+    cat("#",
         composition[[i]],
         dyad_type[[i]],
-        n_dyads[[i]],
-        "\n",
-        sep = "  ")
+        composition_n_dyads[[i]],
+        "dyads\n",
+        sep = " ")
   }
 
-  cat("\n\n")
+  cat("#\n")
 
-  cat("# Added Columns: \n")
+  cat("# Added columns:\n")
 
-  cat("\n\n")
+  if (interdep_composition_col %in% names(x)) {
+    cat("#   .i_composition       inferred dyad composition\n")
+  }
+  if (interdep_composition_role_col %in% names(x)) {
+    cat("#   .i_composition_role  composition-specific member role\n")
+  }
+  if (any(startsWith(names(x), paste0(interdep_reserved_prefix, "is_")))) {
+    cat("#   .i_is_*              composition-role indicator columns\n")
+  }
+  if (interdep_diff_col %in% names(x)) {
+    cat("#   .i_diff              sum-diff contrast for exchangeable dyads\n")
+  }
+  if (any(startsWith(names(x), paste0(interdep_reserved_prefix, "diff_")))) {
+    cat("#   .i_diff_*            composition-specific diff columns\n")
+  }
+
+  cat("#\n")
 
   out <- x
   class(out) <- class(out)[class(out) != "interdep_data"]
